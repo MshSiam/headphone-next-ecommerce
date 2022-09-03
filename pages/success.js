5a103df1b8792398dfb1b0a7e4da61b88b73dfce
MshSiam <siyamkenway@gmail.com>
2022-09-03 22:11:05 +0600

    success page added

---
+++ pages/success.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react"
+import { useStateContext } from "../context/StateContext"
+import { BsBagCheckFill } from "react-icons/bs"
+import Link from "next/link"
+import { runFireworks } from "../lib/utils"
+
+const success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext()
+
+  const [order, setOrder] = useState(null)
+
+  useEffect(() => {
+    localStorage.clear()
+    setCartItems([])
+    setTotalPrice(0)
+    setTotalQuantities(0)
+    runFireworks()
+  }, [])
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill size={100} />
+        </p>
+        <h2>Thank You For Your Order !</h2>
+        <p className="email-msg">
+          Check your email inbox for the payment receipt.
+        </p>
+        <p className="description">
+          If you have any question, please email
+          <a className="email" href="siyamkenway@gmail.com">
+            siyamkenway@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button className="btn" type="button">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default success
