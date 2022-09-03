57ec7dc50c11816c36f7170c7d4ee38fe6ccc7ed
MshSiam <siyamkenway@gmail.com>
2022-09-03 09:35:37 +0600

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,22 @@
 import React from "react"
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiOutlineGithub,
+  AiOutlineLinkedin
+} from "react-icons/ai"
 
 const Footer = () => {
-  return <div>footer</div>
+  return (
+    <div className="footer-container">
+      <p>&copy; Reserved by Muhammad Siam</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiOutlineGithub />
+        <AiOutlineLinkedin />
+      </p>
+    </div>
+  )
 }
-
 export default Footer
